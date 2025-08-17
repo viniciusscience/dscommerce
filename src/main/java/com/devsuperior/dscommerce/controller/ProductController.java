5d073aeccdf21f7d49b2cd97eafd77b9c4aadd57
unknown <daftwarz@gmail.com>
2025-08-17 20:40:05 -0300

    service

--- src/main/java/com/devsuperior/dscommerce/controller/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controller/ProductController.java
@@ -1,24 +1,22 @@
 package com.devsuperior.dscommerce.controller;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.repository.ProductRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.dscommerce.service.ProductService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
-    @GetMapping
-    public String getProducts() {
-        return productRepository
-                .findByName("Smart TV")
-                .getDescription();
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return productService.findById(id);
     }
 }
