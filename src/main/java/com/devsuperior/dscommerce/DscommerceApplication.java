f002455fc0e4db563b8cfd83d84f96421e4bf46f
unknown <daftwarz@gmail.com>
2025-08-19 21:19:13 -0300

    ajuste bcript

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DscommerceApplication {
+    
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscommerceApplication.class, args);
-	}
+        SpringApplication.run(DscommerceApplication.class, args);
+
+    }
 
 }
